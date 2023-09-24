50339f50d4f99d141701934d5855f41bbb023f6c
gabriel-vasconcelos <gabrieltotal6@gmail.com>
2023-09-24 18:24:38 -0300

    Upando a atividade

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -1,24 +1,69 @@
 package com.example.alcoolougasolina
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
+    var resultado:String = "-----"
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putDouble("percentual", percentual)
+        outState.putString("resultado", resultado)
+        super.onSaveInstanceState(outState)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("PDM23","No onCreate, $percentual")
 
+        val campoGasolina: EditText = findViewById(R.id.edGasolina)
+        val campoAlcool: EditText = findViewById(R.id.edAlcool)
+        val switchPercentual: Switch = findViewById(R.id.swPercentual)
         val btCalc: Button = findViewById(R.id.btCalcular)
+        val textViewResultado: TextView = findViewById(R.id.tvResultado)
+
+        if (savedInstanceState != null) {
+            percentual=savedInstanceState.getDouble("percentual")
+            resultado=savedInstanceState.getString("resultado", "-----")
+        }
+        Log.d("PDM23","No onCreate, $percentual, $resultado")
+
+        textViewResultado.text = resultado
+
         btCalc.setOnClickListener(View.OnClickListener {
-            //código do evento
-            percentual=0.75
+            val campoGasolinaValor = campoGasolina.text.toString().toDouble()
+            val campoAlcoolValor = campoAlcool.text.toString().toDouble()
+
             Log.d("PDM23","No btCalcular, $percentual")
+            Log.d("PDM23","Campo Gasolina, $campoGasolinaValor");
+            Log.d("PDM23","Campo Álcool, $campoAlcoolValor");
+
+
+            if(campoAlcoolValor <= campoGasolinaValor * percentual){
+                Log.d("PDM23","Alcool Vale a pena");
+                resultado = "Álcool"
+            }else if (campoAlcoolValor > campoGasolinaValor * percentual){
+                Log.d("PDM23","Gasolina vale a pena");
+                resultado = "Gasolina"
+            }
+            textViewResultado.text = resultado
         })
+
+        switchPercentual.setOnClickListener{
+            if(percentual == 0.7) {
+                percentual = 0.75
+            }else{
+                percentual = 0.7
+             }
+        }
     }
 override fun onResume(){
     super.onResume()
@@ -26,18 +71,18 @@ override fun onResume(){
 }
 override fun onStart(){
     super.onStart()
-    Log.d("PDM23","No onResume")
+    Log.d("PDM23","No onStart")
 }
 override fun onPause(){
     super.onPause()
-    Log.d("PDM23","No onResume")
+    Log.d("PDM23","No onPause")
 }
 override fun onStop(){
     super.onStop()
-    Log.d("PDM23","No onResume")
+    Log.d("PDM23","No onStop")
 }
 override fun onDestroy(){
     super.onDestroy()
-    Log.d("PDM23","No onResume")
+    Log.d("PDM23","No onDestroy")
 }
 }
